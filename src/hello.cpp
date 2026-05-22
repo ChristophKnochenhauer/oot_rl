42529eb40ac1df9580d2bdf3162f008755891f36
Christoph Knochenhauer <69197793+ChristophKnochenhauer@users.noreply.github.com>
2026-05-23 00:31:07 +0200

    EGL context and headless mode

--- src/hello.cpp
+++ src/hello.cpp
@@ -4,10 +4,7 @@
 extern "C" int Ship_Headless_IsEnabled(void);
 
 int main(int argc, char** argv) {
-    std::cout << "Before SetHeadless: " << Ship_Headless_IsEnabled() << "\n";
     SoH_SetHeadless(1);
-    std::cout << "After SetHeadless: " << Ship_Headless_IsEnabled() << "\n";
-    
     SoH_Init(argc, argv);
     for (int i = 0; i < 5; i++) {
         SoH_StepFrame();
