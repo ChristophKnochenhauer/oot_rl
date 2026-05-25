d7326144df7c74d2664890fc348c512a933e1984
Christoph Knochenhauer <69197793+ChristophKnochenhauer@users.noreply.github.com>
2026-05-25 17:14:17 +0200

    Add input controls

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,15 +1,30 @@
-#include <iostream>
-#include <soh_lib.h>
-
-extern "C" int Ship_Headless_IsEnabled(void);
+#include "soh_lib.h"
+#include <stdio.h>
 
 int main(int argc, char** argv) {
     SoH_SetHeadless(1);
     SoH_Init(argc, argv);
-    for (int i = 0; i < 5; i++) {
+
+    SoH_Input pressed_a = {
+        .buttons = 0x8000,
+        .stick_x = 0,
+        .stick_y = 0,
+        .right_stick_x = 0,
+        .right_stick_y = 0,
+    };
+    SoH_SetInput(0, &pressed_a);
+
+    for (int i = 0; i < 120; i++) {
         SoH_StepFrame();
-        std::cout << "Frame " << i << " done\n";
     }
+    
+    printf("[hello] 120 frames with synthetic input A. Clearing.\n");
+    SoH_ClearInput(0);
+
+    for (int i = 0; i < 60; i++) {
+        SoH_StepFrame();
+    }
+
     SoH_Shutdown();
     return 0;
 }
